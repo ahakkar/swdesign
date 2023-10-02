0652d34428a8eb3a9fdcc58dc9ae9261d016e267
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-10-02 21:17:57 +0300

    align controllers according to module chart

--- src/main/java/org/example/controller/PrimaryController.java
+++ src/main/java/org/example/controller/PrimaryController.java
@@ -3,12 +3,16 @@ package org.example.controller;
 import java.io.IOException;
 
 import org.example.App;
+import org.example.controller.factory.ChartFactory;
+import org.example.model.data.ApiDataRequest;
+import org.example.model.data.ApiDataResult;
 import org.example.types.Scenes;
 import org.example.utils.EnvironmentVariables;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 /**
@@ -45,6 +49,7 @@ public class PrimaryController {
         EnvironmentVariables.load(".env");
 
         LoadScene(Scenes.MainWorkspace.toString());
+        registerAsObserver();
     }
 
        
@@ -59,4 +64,41 @@ public class PrimaryController {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
+
+
+    /**
+     * Register primarycontroller as datamanager's observer, so datamanager
+     * can notify listeners when the data is ready
+     */
+    private void registerAsObserver() {
+        // TODO create new datamanager instance?
+        // TODO register itself as an observer in datamanager
+    }
+
+
+    /**
+     * Is called when datamanager has the data on hand and ready to be used.
+     * PrimaryController must be registered as datamanager's observer beforehand.
+     */
+    public void onDataReady() {
+        // Fetch the data and update the view
+        // createChart();
+    }
+
+
+    /**
+     * Get chart data from datamanager, use ChartFactory to generate chart,
+     * command RequestController to display it
+     */
+    private void createChart() {
+        // TODO get chart data from datamanager
+        //ApiDataResult chartData = primaryService.getApiData();
+
+        // TODO use chartfactory to generate a new chart based on the data
+        //ChartFactory cf = new ChartFactory(); 
+        //XYChart<?, ?> chart = cf.generateChart(chartData);
+
+        // TODO update view with chart
+            
+    };
 }
