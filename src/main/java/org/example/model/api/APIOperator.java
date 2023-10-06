69fa37550043d87c5034150fe553de5b9c6c7e83
unknown <markus.hissa@tuni.fi>
2023-10-06 14:28:29 +0300

    Axis datatypes are now in its own class
    Closes #40

--- src/main/java/org/example/model/api/APIOperator.java
+++ src/main/java/org/example/model/api/APIOperator.java
@@ -1,12 +1,17 @@
 package org.example.model.api;
 
-import org.example.model.data.*;
+import java.io.IOException;
+
+import org.example.model.data.ApiDataRequest;
+import org.example.model.data.ApiDataResult;
+import org.example.model.data.EnergyModel;
+import org.example.model.data.WeatherModel;
 
 public class APIOperator {
 
-    protected static ApiDataResult getData(ApiDataRequest request) throws InterruptedException {
+    protected static ApiDataResult getData(ApiDataRequest request) throws InterruptedException, IOException {
         if (request.getDataClass() == WeatherModel.class) {
-            // Tästä voidaan päätellä, että kutsutaan Ilmatieteenlaitoksen APIa @MARKUS
+
         } else if (request.getDataClass() == EnergyModel.class) {
             // Tästä voidaan päätellä, että kutsutaan Fingridin APIa @HEIKKI
         }
--- src/main/java/org/example/types/AxisType.java
+++ src/main/java/org/example/types/AxisType.java
@@ -9,24 +9,59 @@ import java.util.List;
  * @author Antti Hakkarainen 
  */
 public enum AxisType {
-    CONSUMPTION("Consumption", "124", "Electricity Consumption (1 Hour)"),
-    PRODUCTION("Production", "74", "Electricity Production (1 Hour)"),
-    PRICE("Price", "00", ""),
-    TEMPERATURE("Temperature", "00", ""),
-    WIND("Wind", "00", "");
+
+    CONSUMPTION("Consumption", true, true, true, true, false, APIType.FINGRID, "Electricity Consumption (1 Hour)"),
+    PRODUCTION("Production", true, true, true, true, true, APIType.FINGRID,  "Electricity Production (1 Hour)"),
+    PRICE("Price", true, true, true, true, true, null, "Price"),
+    TEMPERATURE("Temperature", true, true, true, true, true, APIType.FMI, "Temperature (1 Hour)"),
+    WIND("Wind", true, true, true, true, true, APIType.FMI, "Wind (1 Hour)"),
+    RAIN("Rain", true, true, true, true, true, APIType.FMI, "Rain (1 Hour)"),
+    HUMIDITY("Humidity", true, true, true, true, true, APIType.FMI, "Humidity (1 Hour)"),
+    TIME("Time", true, false, true, true, true, null, "Time (1 Hour)"),
+    AIR_PRESSURE("Air pressure", true, true, true, true, true, APIType.FMI, "Air pressure (1 Hour)");
 
     private final String label;
-    private final String variableId;
+    private final boolean xAxisAllowed;
+    private final boolean yAxisAllowed;
+    private final boolean lineChartAllowed;
+    private final boolean scatterChartAllowed;
+    private final boolean pieChartAllowed;
+    private final APIType api;
     private final String description;
 
-    AxisType(String label, String variableId, String description) {
+    AxisType(String label, boolean xAxisAllowed, boolean yAxisAllowed, boolean lineChartAllowed, boolean scatterChartAllowed, boolean pieChartAllowed, APIType api, String description) {
         this.label = label;
-        this.variableId = variableId;
+        this.xAxisAllowed = xAxisAllowed;
+        this.yAxisAllowed = yAxisAllowed;
+        this.lineChartAllowed = lineChartAllowed;
+        this.scatterChartAllowed = scatterChartAllowed;
+        this.pieChartAllowed = pieChartAllowed;
+        this.api = api;
         this.description = description;
     }
 
-    public String getVariableId() {
-        return variableId;
+    public boolean isXAxisAllowed() {
+        return xAxisAllowed;
+    }
+
+    public boolean isYAxisAllowed() {
+        return yAxisAllowed;
+    }
+
+    public boolean isLineChartAllowed() {
+        return lineChartAllowed;
+    }
+
+    public boolean isScatterChartAllowed() {
+        return scatterChartAllowed;
+    }
+
+    public boolean isPieChartAllowed() {
+        return pieChartAllowed;
+    }
+
+    public APIType getApi() {
+        return api;
     }
 
     public String getDescription() {
