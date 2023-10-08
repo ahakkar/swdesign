004efce89ac5fc952f27dc5518291df2cc9d6acf
unknown <markus.hissa@tuni.fi>
2023-10-08 18:14:53 +0300

    Code for making FMI requests in APIOperator

--- src/main/java/org/example/model/api/APIOperator.java
+++ src/main/java/org/example/model/api/APIOperator.java
@@ -1,16 +1,48 @@
 package org.example.model.api;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.example.model.data.ApiDataRequest;
 import org.example.model.data.ApiDataResult;
 import org.example.model.data.EnergyModel;
 import org.example.model.data.WeatherModel;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class APIOperator {
 
-    protected static ApiDataResult getData(ApiDataRequest request) throws InterruptedException, IOException {
+    protected static ApiDataResult getData(ApiDataRequest request) throws InterruptedException, IOException, ParserConfigurationException, SAXException {
         if (request.getDataClass() == WeatherModel.class) {
+                    ArrayList<String> values = new ArrayList<>();
+        URL url = new URL("http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::harmonie::surface::point::multipointcoverage&place=alavus&parameters=temperature");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = dbf.newDocumentBuilder();
+
+        Document document = builder.parse(connection.getInputStream());
+
+        document.getDocumentElement().normalize();
+
+        NodeList data = document.getElementsByTagName("gml:doubleOrNilReasonTupleList");
+
+        System.out.println(data);
+        NodeList subData = data.item(0).getChildNodes();
+
+        for ( int i = 0; i < subData.getLength(); ++i )
+        {
+            values.add(subData.item(i).getNodeValue());
+        }
 
         } else if (request.getDataClass() == EnergyModel.class) {
             // Tästä voidaan päätellä, että kutsutaan Fingridin APIa @HEIKKI
